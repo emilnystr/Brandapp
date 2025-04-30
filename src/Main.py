312b76722ab8659952f093154c279a38d64b70cf
Emil Nyström <emil.nystromtyrens.se>
2025-04-30 10:50:58 +0200

    added framstegsindikator

--- src/Main.py
+++ src/Main.py
@@ -11,14 +11,8 @@ from Parametrisk_brandkurva import*
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
 
- 
-
-
-
-
 materialdata = skapa_indata()
 element_sizes, element_material_indices, antal_noder = skapa_element(materialdata)
-
 
 initial_temperature = config["initial_temperature"]
 simuleringstid = config["simulation_time"]
@@ -30,12 +24,12 @@ dt = config["time_step"]
 brandkurva = config["Fire_curve"]
 Constant_surface_temperature = config["constant_surface_temperature"]
 
-
-
-
 T = np.full(antal_noder, initial_temperature, dtype=np.float32)
 Q = np.zeros(antal_noder, dtype=np.float32)
 antal_tidssteg = int(simuleringstid / dt)
+
+# Beräknar hur ofta vi ska uppdatera status (var 1%)
+uppdatera_tid = max(1, antal_tidssteg // 50)
 
 start_time = time.time()
 for i in range(antal_tidssteg):
@@ -46,14 +40,11 @@ for i in range(antal_tidssteg):
     elif brandkurva == 2:
         T_fire = HC180(tid)
     elif brandkurva == 3:
-        T_fire = parametrisk_brand(tid) #tänk på att den parametriska branden återgår till 20 grader efter en tid, beror på brandbelastningen, så man kan inte simulera värmeledningen hur länge som helst med denna brandkurva
+        T_fire = parametrisk_brand(tid)
     elif brandkurva == 4:
         T_fire = Constant_surface_temperature
-
-    
     else:
         print("du har angett en felaktig brandkurva")
-
     
     K, C = beräkna_matriser(element_sizes, element_material_indices, material_databas, T)
     
@@ -61,9 +52,13 @@ for i in range(antal_tidssteg):
     Q[-1] = h * (T_g - T[-1]) + sigma * epsilon * ((T_g + 273.15)**4 - (T[-1] + 273.15)**4)
     
     T += dt * (Q - K @ T) / C
+    
+    #framstegsindikatorn
+    if i % uppdatera_tid == 0 or i == antal_tidssteg - 1:
+        procent_klar = (i + 1) / antal_tidssteg * 100
+        print(f"\rSimuleringen är {procent_klar:.1f}% klar", end="", flush=True)
 
-
-print(f"Simuleringen tog {time.time() - start_time:.2f} sekunder")
+print(f"\nSimuleringen tog {time.time() - start_time:.2f} sekunder")
 
 plot_results(element_sizes, T)
 print(T)
